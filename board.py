0297f0e62934a2cc15de9adccb24a1ccf2bb4a13
nandadev <nandadev.rajeevmenon@student.griffith.ie>
2023-11-29 11:46:29 +0000

    Init

---
+++ board.py
@@ -0,0 +1,122 @@
+from PyQt6.QtWidgets import QFrame
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal
+from PyQt6.QtGui import QPainter, QColor, QBrush
+from PyQt6.QtCore import QPoint
+
+
+class Board(QFrame):  # base the board on a QFrame widget
+    updateTimerSignal = pyqtSignal(int)  # signal sent when the timer is updated
+    clickLocationSignal = pyqtSignal(
+        str
+    )  # signal sent when there is a new click location
+
+    # TODO set the board width and height to be square
+    boardWidth = 10  # board is 0 squares wide # TODO this needs updating
+    boardHeight = 10  #
+    timerSpeed = 1000  # the timer updates every 1 second
+    counter = 10  # the number the counter will count down from
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.initBoard()
+
+    def initBoard(self):
+        """initiates board"""
+        self.timer = QTimer(self)  # create a timer for the game
+        self.timer.timeout.connect(
+            self.timerEvent
+        )  # connect timeout signal to timerEvent method
+        self.isStarted = False  # game is not currently started
+        self.start()  # start the game which will start the timer
+
+        self.boardArray = (
+            []
+        )  # TODO - create a 2d int/Piece array to store the state of the game
+        # self.printBoardArray()    # TODO - uncomment this method after creating the array above
+
+    def printBoardArray(self):
+        """prints the boardArray in an attractive way"""
+        print("boardArray:")
+        print(
+            "\n".join(
+                ["\t".join([str(cell) for cell in row]) for row in self.boardArray]
+            )
+        )
+
+    def mousePosToColRow(self, event):
+        """convert the mouse click event to a row and column"""
+        pass  # Implement this method according to your logic
+
+    def squareWidth(self):
+        """returns the width of one square in the board"""
+        return self.contentsRect().width() / self.boardWidth
+
+    def squareHeight(self):
+        """returns the height of one square of the board"""
+        return self.contentsRect().height() / self.boardHeight
+
+    def start(self):
+        """starts game"""
+        self.isStarted = (
+            True  # set the boolean which determines if the game has started to TRUE
+        )
+        self.resetGame()  # reset the game
+        self.timer.start(self.timerSpeed)  # start the timer with the correct speed
+        print("start () - timer is started")
+
+    def timerEvent(self):
+        """this event is automatically called when the timer is updated. based on the timerSpeed variable"""
+        # TODO adapt this code to handle your timers
+        if Board.counter == 0:
+            print("Game over")
+        self.counter -= 1
+        print("timerEvent()", self.counter)
+        self.updateTimerSignal.emit(self.counter)
+
+    def paintEvent(self, event):
+        """paints the board and the pieces of the game"""
+        painter = QPainter(self)
+        self.drawBoardSquares(painter)
+        self.drawPieces(painter)
+
+    def mousePressEvent(self, event):
+        """this event is automatically called when the mouse is pressed"""
+        clickLoc = (
+            "click location [" + str(event.x()) + "," + str(event.y()) + "]"
+        )  # the location where a mouse click was registered
+        print("mousePressEvent() - " + clickLoc)
+        # TODO you could call some game logic here
+        self.clickLocationSignal.emit(clickLoc)
+
+    def resetGame(self):
+        """clears pieces from the board"""
+        # TODO write code to reset game
+
+    def tryMove(self, newX, newY):
+        """tries to move a piece"""
+        pass  # Implement this method according to your logic
+
+    def drawBoardSquares(self, painter):
+        """draw all the square on the board"""
+        squareWidth = int(self.squareWidth())
+        squareHeight = int(self.squareHeight())
+        for row in range(0, Board.boardHeight):
+            for col in range(0, Board.boardWidth):
+                painter.save()
+                painter.translate(col * squareWidth, row * squareHeight)
+                painter.setBrush(QBrush(QColor(0, 0, 0)))  # Set brush color
+                painter.drawRect(0, 0, squareWidth, squareHeight)  # Draw rectangles
+                painter.restore()
+
+    def drawPieces(self, painter):
+        """draw the pieces on the board"""
+        for row in range(0, len(self.boardArray)):
+            for col in range(0, len(self.boardArray[0])):
+                painter.save()
+                painter.translate(col * self.squareWidth(), row * self.squareHeight())
+                # TODO draw some pieces as ellipses
+                # TODO choose your color and set the painter brush to the correct color
+                radius = (self.squareWidth() - 2) / 2
+                center = QPoint(radius, radius)
+                painter.drawEllipse(center, radius, radius)
+                painter.restore()
---
+++ go.py
@@ -0,0 +1,46 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDockWidget
+from PyQt6.QtCore import Qt
+
+from board import Board
+from score_board import ScoreBoard
+
+
+class Go(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def getBoard(self):
+        return self.board
+
+    def getScoreBoard(self):
+        return self.scoreBoard
+
+    def initUI(self):
+        '''Initiates application UI'''
+        self.board = Board(self)
+        self.setCentralWidget(self.board)
+
+        self.scoreBoard = ScoreBoard()
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
+        self.scoreBoard.make_connection(self.board)
+
+        self.resize(800, 800)
+        self.center()
+        self.setWindowTitle('Go')
+        self.show()
+
+
+    def center(self):
+        '''Centers the window on the screen'''
+        screen = QApplication.primaryScreen().availableGeometry()
+        size = self.geometry()
+        x = (screen.width() - size.width()) // 2
+        y = (screen.height() - size.height()) // 2
+        self.move(x, y)
+
+
+
+
+
