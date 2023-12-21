4b8a1cd1a02af890eaaba46e7a2f7f843db413bc
nandadev <nandadev.rajeevmenon@student.griffith.ie>
2023-12-21 19:13:45 +0000

    Comments for future

--- game_logic.py
+++ game_logic.py
@@ -10,11 +10,17 @@ class GameLogic:
             board[i].append(Piece(i, j, board))
 
     def place_piece(color, y, x):
-        # check suicide
-        # check KO
         # check is theres a piece there
+        # check suicide
         # no piece
+        # check liberties
+        # check KO
+
+        # add it to group
+
         pass
+
+    # for groups, check how to know whcih group a piece is in. Then for every cpature you will have a function that
 
 
 logic = GameLogic()
