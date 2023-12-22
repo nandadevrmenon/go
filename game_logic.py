e9a22aed665819bc80bfc8ff9dfb156030987315
nandadev <nandadev.rajeevmenon@student.griffith.ie>
2023-12-22 16:43:31 +0000

    Basic capturing added'

--- game_logic.py
+++ game_logic.py
@@ -2,14 +2,42 @@ from piece import Piece
 
 
 class GameLogic:
+    turn = 0
+    board_states = []
     board = []
+    all_groups = []
 
-    for i in range(8):
+    for i in range(7):
         board.append([])
-        for j in range(8):
-            board[i].append(Piece(i, j, board))
+        for j in range(7):
+            board[i].append(Piece(i, j, board, all_groups))
 
-    def place_piece(color, y, x):
+    def try_move(type, y, x):
+        board = GameLogic.board
+        piece = board[y][x]
+        if piece.type != 0:  # already a piece there
+            return None
+        if piece.liberties == 0:
+            if piece.has_all_nieghbours_of_type(type):
+                GameLogic.place_piece(type, piece)
+                GameLogic.decrement_neighbour_liberties(piece)
+                piece.add_to_group()
+                GameLogic.check_captures_near(piece)
+                return True
+            else:  # it is suicide
+                GameLogic.display_wrong_move()
+                return False
+        else:
+            if GameLogic.check_for_KO():
+                GameLogic.display_wrong_move()
+                return False
+            else:
+                GameLogic.place_piece(type, piece)
+                GameLogic.decrement_neighbour_liberties(piece)
+                piece.add_to_group()
+                GameLogic.check_captures_near(piece)
+                return True
+
         # check is theres a piece there
         # check suicide
         # no piece
@@ -20,9 +48,51 @@ class GameLogic:
 
         pass
 
+    def display_wrong_move():
+        print("Thats was a wrong move ")
+
+    def check_for_KO():
+        print("proint checking for KO")
+        return False
+
+    def place_piece(type, piece):
+        piece.type = type
+        print("placing piece")
+
+    # def remove_piece(piece):
+    #     piece.type = 0
+    #     piece.group = None
+
+    def decrement_neighbour_liberties(piece):
+        neighbours = piece.get_neighbours()
+        for piece in neighbours:
+            piece.liberties = piece.liberties - 1
+
+    def check_captures_near(piece):
+        neighbours = piece.get_neighbours()
+        groups = [piece.group for piece in neighbours if piece.group is not None]
+        for group in groups:
+            if not (group.check_for_life()):
+                print("group found dead")
+                removed_pieces = group.remove()
+
+    def print_board():
+        for row in GameLogic.board:
+            for element in row:
+                print(element.type, end=" ")  # Print each element in the row
+            print()  # Move to the next line for the next row
+
     # for groups, check how to know whcih group a piece is in. Then for every cpature you will have a function that
 
 
 logic = GameLogic()
 
-print(logic.board[6][0].calculate_liberties())
+black = True
+while True:
+    # Get row and column input
+    row_input = int(input("Enter y value the row()"))
+    column_input = int(input("Enter x value which is the column "))
+
+    if GameLogic.try_move(1 if black else 2, row_input, column_input):
+        black = not black
+    GameLogic.print_board()
--- piece.py
+++ piece.py
@@ -1,10 +1,12 @@
-class Piece:
-    def __init__(self, y, x, board):
+class Piece(object):
+    def __init__(self, y, x, board, all_groups):
         self.board = board
         self.y = y
         self.x = x
         self.type = 0  # 0 is emtpy / 1 is black / 2 is white
         self.liberties = None
+        self.all_groups = all_groups
+        self.group = None
 
         self.position_type = (
             self.find_position_type()
@@ -52,3 +54,78 @@ class Piece:
             neighbours.append(self.board[position[0]][position[1]])
 
         return neighbours
+
+    def has_all_nieghbours_of_type(self, type):
+        neighbours = self.get_neighbours()
+        for neighbour in neighbours:
+            if neighbour.type is not type:
+                return False
+        return True
+
+    def add_to_group(self):
+        # finds neighbouring groups to add itself to ir creates a new group on its own
+        neighbour_groups = []
+        neighbours = self.get_neighbours()
+        for piece in neighbours:
+            if (
+                piece.type == self.type
+                and piece.group is not None
+                and piece.group not in neighbour_groups
+            ):
+                neighbour_groups.append(piece.group)
+        if not neighbour_groups:
+            group = Group(self.board, self.all_groups, self)
+            return group
+        else:
+            if len(neighbour_groups) > 1:
+                for group in neighbour_groups[1:]:
+                    neighbour_groups[0].merge(group)
+            neighbour_groups[0].stones.append(self)
+            return neighbour_groups[0]
+
+    def remove(self):
+        self.type = 0
+        self.group = None
+
+
+class Group(object):
+    def __init__(self, board, all_groups, stone):
+        self.all_groups = all_groups
+        self.all_groups.append(self)
+        self.stones = [stone]
+        stone.group = self
+
+    def merge(self, group):
+        """Merge two groups.
+
+        This method merges the argument group with this one by adding
+        all its stones into this one. After that it removes the group
+        from the board.
+
+        Arguments:
+        group -- the group to be merged with this one
+
+        """
+        for stone in group.stones:
+            stone.group = self
+            self.stones.append(stone)
+        self.all_groups.remove(group)
+        del group
+
+    def remove(self):
+        """Remove the entire group."""
+        while self.stones:
+            self.stones[0].remove()
+            del self.stones[0]
+        self.all_groups.remove(self)
+        del self
+
+    def check_for_life(self):
+        for stone in self.stones:
+            if stone.liberties > 0:
+                return True
+        return False
+
+    def __str__(self):
+        """Return a list of the group's stones as a string."""
+        return str([str(stone) for stone in self.stones])
