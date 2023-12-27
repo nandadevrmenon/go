18513841f83d79c1c30a4031df3a81b8ec315708
nandadev <nandadev.rajeevmenon@student.griffith.ie>
2023-12-27 18:38:20 +0000

    update buttons in game eng dialogue

--- GameEndDialogue.py
+++ GameEndDialogue.py
@@ -11,16 +11,16 @@ from PyQt6 import QtCore
 
 from game_logic import GameLogic
 
+
 class GameEndDialog(QDialog):
     def __init__(
-        self,
-        player1,
-        player2,
+        self, rematch_callback, back_to_start_signal
     ):  # takes all the palyer info
         super().__init__()
-
-        self.player1 = player1
-        self.player2 = player2
+        self.rematch_callback = rematch_callback
+        self.back_to_start_signal = back_to_start_signal
+        self.player1 = GameLogic.player1
+        self.player2 = GameLogic.player2
 
         # set the main window stuff
         self.setWindowTitle("Game Over")
@@ -42,7 +42,8 @@ class GameEndDialog(QDialog):
         score_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         score_label.setStyleSheet("color: white;")
 
-        quit_button = PrimaryButton("Quit", self.quit_application)
+        rematch_button = PrimaryButton("Rematch", self.rematch)
+        new_game_button = PrimaryButton("New Game", self.new_game)
 
         # ad the widgets to the main layout
         layout.addWidget(label, 0, 0, 1, 2)
@@ -53,28 +54,37 @@ class GameEndDialog(QDialog):
         medal_image.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(medal_image, 3, 0, 1, 2)  # add the medal icon
         label.setStyleSheet("QLabel { line-height: 0px;color:white; }")
-        layout.addWidget(quit_button, 4, 0, 1, 2)
+        layout.addWidget(rematch_button, 4, 0, 1, 1)
+        layout.addWidget(new_game_button, 4, 1, 1, 1)
         layout.update()  # to make sure empty rows are squshed
 
-    def set_winner_text(self, label):  # sets text based on which player won the game or if its a draw , it will show that
+    def set_winner_text(
+        self, label
+    ):  # sets text based on which player won the game or if its a draw , it will show that
         p1_score, p2_score = self.get_end_score()
 
         if p1_score > p2_score:
             winner_name = self.player1["name"]
         else:
             winner_name = self.player2["name"]
-        
+
         label.setText(winner_name + "\nwins the game!")
 
-    def get_end_score(self):  #get end score
+    def get_end_score(self):  # get end score
         p1_territory, p2_territory = GameLogic.calculate_territories()
 
-        p1_score = self.player1["score"][0] + self.player1["score"][1] + p1_territory # stones on board + territories 
-        p2_score = self.player2["score"][0] + self.player2["score"][1] + p2_territory + 7.5 #7.5 kumi for white player
+        p1_score = (
+            self.player1["score"][0] + self.player1["score"][1] + p1_territory
+        )  # stones on board + territories
+        p2_score = (
+            self.player2["score"][0] + self.player2["score"][1] + p2_territory + 7.5
+        )  # 7.5 kumi for white player
 
         return p1_score, p2_score
 
-    def set_score_text(self, score_label):  # sets text based on which player won the game or if its a draw , it will show that
+    def set_score_text(
+        self, score_label
+    ):  # sets text based on which player won the game or if its a draw , it will show that
         p1_score, p2_score = self.get_end_score()
         p1_territory, p2_territory = GameLogic.calculate_territories()
 
@@ -88,8 +98,25 @@ class GameEndDialog(QDialog):
             captured = self.player2["score"][1]
             territory = p2_territory
             total_score = p2_score
-        
-        score_label.setText("Stones: " + str(stone) + "\nCaptured: "+ str(captured) + "\nTerritories: "+ str(territory) + "\n Total: "+ str(total_score))
+
+        score_label.setText(
+            "Stones: "
+            + str(stone)
+            + "\nCaptured: "
+            + str(captured)
+            + "\nTerritories: "
+            + str(territory)
+            + "\n Total: "
+            + str(total_score)
+        )
+
+    def rematch(self):
+        self.close()
+        self.rematch_callback()
+
+    def new_game(self):
+        self.close()
+        self.back_to_start_signal.emit()
 
     def get_statliches(self):  # to get the font from the file
         font_path = QtCore.QDir.currentPath() + "/fonts/statliches.ttf"
@@ -100,16 +127,3 @@ class GameEndDialog(QDialog):
         else:
             print("font not found ")
             return "Helvetica"
-
-    def get_josefin(self):  # to get the font from the file
-        font_path = QtCore.QDir.currentPath() + "/fonts/josefin.ttf"
-        font_id = QFontDatabase.addApplicationFont(font_path)
-        # Check if the font was loaded successfully
-        if font_id != -1:
-            return QFontDatabase.applicationFontFamilies(font_id)[0]
-        else:
-            print("font not found ")
-            return "Helvetica"
-        
-    def quit_application(self):
-        QApplication.quit()
